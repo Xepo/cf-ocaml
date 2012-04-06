f7dc4cb3b3d5e1ff629bdc81ae3f0a46032ef330
Isaiah Damron <xepo@trifault.net>
2012-04-06 05:09:46 -0400

    Success for id2!

--- s1.ml
+++ s1.ml
@@ -40,8 +40,34 @@ let rec t1 () =
      many 4 (rot 90.) (shape t1 *- (tra ~x:2.0 ~s:0.4 ()))
 
 
+(* t2 *)
+let rec sq () =
+     begin 
+     choose
+     [
+          0.9, (square ++ 
+               (square *- (tra ~w:1.2 ~h:0.65 ())));
+
+          0.1, (shape r *- (sc 0.5 0.5)) ++
+               (shape r *- (flip_y) *- (sc 0.5 0.5)) ++
+               square ++
+               (square *- (tra ~s:0.7 ()));
+     ]
+     ()
+     end
+
+and r () =
+     many 80 (tra ~x:1.0 ~rot:1.0 ~s:0.98 ()) (shape sq)
 
 
+let m () =
+     (circle *- (v 9) *- (sc 38. 38.)) ++ (*brightness*)
+     (circle *- (sc 39. 39.)) ++
+     many 8 (rot 45.)
+          (shape r *- (tra ~x:20. ~s:2. ()))
+
+let t2 () =
+     many 5 (tra ~s:0.5 ~rot:11. ()) (shape m)
 
      (*TODO: *- v 9 on surround isn't working*)
-let _ = Scene.render_scene ~w:700 ~h:700 ~alias:3 ~bg:9 t1
+let _ = Scene.render_scene ~w:700 ~h:700 ~alias:3 ~bg:9 t2
